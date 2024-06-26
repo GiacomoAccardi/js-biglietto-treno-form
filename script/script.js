64d63490abf1549703a3eb323b87ac4c642b07dd
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-26 15:12:33 +0200

    [FEAT] Our script can now "take" the infos from the users

--- script/script.js
+++ script/script.js
@@ -1,2 +1,10 @@
 //::::::Benvenuti nel mio script!::::::
 
+// Come prima cosa, andiamo a prelevare i dati inseriti dall'utente!
+const button = document.getElementById('start_script');
+
+button.addEventListener('click', function () {
+    let user_name = document.getElementById('user_name').value;
+    let travel_distance = document.getElementById('travel_distance').value;
+    let user_age = document.getElementById('user_age').value;
+});
