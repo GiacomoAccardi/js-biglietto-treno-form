181d53ff4e8996f911e25c8dd932fc7510c47425
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-26 15:22:58 +0200

    [UPD] our script now works and gives the final price of the ticket in console

--- script/script.js
+++ script/script.js
@@ -7,10 +7,25 @@ button.addEventListener('click', function () {
     let user_name = document.getElementById('user_name').value;
     let travel_distance = document.getElementById('travel_distance').value;
     let user_age = document.getElementById('user_age').value;
-});
 
 // Definisco il prezzo del biglietto in base alla distanza da percorrere
 
-let ticket_price = 0.21 * travel_distance;
+    let ticket_price = 0.21 * travel_distance;
 
-let final_price = ticket_price;
+    let final_price = ticket_price;
+
+// Applichiamo lo sconto in base all'età
+
+    let price_discount;
+
+    if (user_age == 'minorenne') {
+            price_discount = ticket_price * 0.2;
+            final_price = (ticket_price - price_discount);
+    } else if (user_age == 'over65') {
+            price_discount = ticket_price * 0.4   
+            final_price = (ticket_price - price_discount)
+    }
+
+  console.log(final_price);
+});
+
